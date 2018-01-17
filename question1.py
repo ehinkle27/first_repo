148dda5d35e40f6ffc644f4d6db01f5704176f81
ehinkle27@hotmail.com <ehinkle27@hotmail.com>
2018-01-16 21:04:10 -0700

    Inital files

---
+++ question1.py
@@ -0,0 +1,6 @@
+number = 0
+number = int(input("Enter a number: "))
+If number = 0:
+    print("the inputed number is zero")
+elif number > 0:
+    print("the input number is positive
---
+++ question5.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number: "))
+factor = number
+while number <= 0
+    number = number - 1
+    factor = factor*number     
+print(factor)
